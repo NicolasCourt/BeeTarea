01574399df36dc7181dee4bec807f2c65797523c
NicolasCourt <ncourt@uc.cl>
2021-12-03 02:22:19 -0300

    minor changes :rocket:

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -1,8 +1,10 @@
-class Auto < ApplicationRecord
+class Auto < ApplicationRecord # validacion de datos ingresados
     validates :lat, presence: true,
-                numericality: {only_float: true}
+                numericality: { only_float: true, greater_than_or_equal_to: -90, less_than_or_equal_to: 90 } 
+
     validates :lng, presence: true,
-                numericality: {only_float: true}
+                numericality: { only_float: true, greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
     validates :patente, presence: true,
-                length: { minimum: 1 }
+                length: { minimum: 1, maximum: 8 }
 end
